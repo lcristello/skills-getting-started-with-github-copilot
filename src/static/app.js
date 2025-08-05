7bb561b62fd27db6bf85ce41467a7efe3563ebf3
lcristello <lcristello@technolutions.com>
2025-08-05 18:53:38 +0000

    Show active participants

--- src/static/app.js
+++ src/static/app.js
@@ -25,6 +25,11 @@ document.addEventListener("DOMContentLoaded", () => {
           <p>${details.description}</p>
           <p><strong>Schedule:</strong> ${details.schedule}</p>
           <p><strong>Availability:</strong> ${spotsLeft} spots left</p>
+          <p><strong>Participants:</strong> ${
+            details.participants.length > 0
+              ? details.participants.map(email => `<span>${email}</span>`).join(", ")
+              : "None yet"
+          }</p>
         `;
 
         activitiesList.appendChild(activityCard);
